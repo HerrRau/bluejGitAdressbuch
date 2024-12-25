16d51b3ab4e543ce8e5e93ef1064984f0a8ee334
Herr Rau <schule@herr-rau.de>
2024-12-25 09:33:27 +0100

    doch schon alle Methoden zu Adress ehinzugefügt

--- Adresse.java
+++ Adresse.java
@@ -1,5 +1,66 @@
-public class Adresse
+public class Adresse implements Datenelement
 {
-   
+    private String nachname;
+    private String vorname;
+    private String email;
+    private String telefon;
+    
+    public Adresse(String nachname, String vorname, String email, String telefon)
+    {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.email = email;
+        this.telefon = telefon;
+    }
+    
+    public void emailAendern(String emailNeu) {
+        this.email = emailNeu;
+    }
+    public void telefonAendern(String telefonNeu) {
+        this.telefon = telefonNeu;
+    }
+    public String nachnameGeben() {
+        return nachname;
+    }
+    public String vornameGeben() {
+        return vorname;
+    }
+    public String emailGeben() {
+        return email;
+    }
+    public String telefonGeben() {
+        return telefon;
+    }
+    
+    public String schluesselAlsStringGeben() {
+        return nachname + ", " + vorname;
+    }
+    
+    // Methoden, die als Datenelement gefordert sind
+    
+    public void informationAusgeben() {
+    }
+    
+    public boolean schluesselIstGleich(String s) {
+        return s.equals(this.schluesselAlsStringGeben());
+    }
+
+    public boolean schluesselIstGroesserAls(String s) {
+        return s.compareTo(this.schluesselAlsStringGeben()) <= 0;        
+    }
+
+    public boolean istKleinerAls(Datenelement d) {
+        Adresse a = (Adresse) d;
+        return this.schluesselAlsStringGeben().compareTo(a.schluesselAlsStringGeben()) < 0;   
+    }
+    public boolean istGroesserAls(Datenelement d) {
+                Adresse a = (Adresse) d;
+        return this.schluesselAlsStringGeben().compareTo(a.schluesselAlsStringGeben()) > 0;   
+    }
+    public boolean istGleich(Datenelement d) {
+        Adresse a = (Adresse) d;
+        return this.schluesselAlsStringGeben().compareTo(a.schluesselAlsStringGeben()) == 0;           
+    }
+
     
 }
--- Model.java
+++ Model.java
@@ -2,7 +2,6 @@ public interface Model
 {
     void adresseEintragen(Adresse a);    
     Adresse adresseSuchen(String suchschluessel);
-
     void emailAendern(String suchschluessel, String email);
     void telefonAendern(String suchschluessel, String telefon);
     
